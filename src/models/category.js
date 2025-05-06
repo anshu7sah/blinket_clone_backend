728f98baba1d40c2c3698227e5327b2ad01fa7bf
Anshu Sah <anshu7sah@gmail.com>
2025-05-06 11:37:32 +0545

    setup server and created user, product, category schema

---
+++ src/models/category.js
@@ -0,0 +1,10 @@
+const { default: mongoose } = require("mongoose");
+
+const categorySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+});
+
+const Category = mongoose.model("Category", categorySchema);
+
+export default Category;
