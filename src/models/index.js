3b664afeaf5d7a1dba015c7f6de6caa9fa218f09
Anshu Sah <anshu7sah@gmail.com>
2025-05-06 13:08:15 +0545

    added Branch Counter and Order model

---
+++ src/models/index.js
@@ -0,0 +1,17 @@
+import Order from "./order.js";
+import Branch from "./branch.js";
+import Counter from "./counter.js";
+import { Customer, DeliveryPartner, Admin } from "./user.js";
+import Product from "./products.js";
+import Category from "./category.js";
+
+export {
+  Order,
+  Counter,
+  Branch,
+  Customer,
+  DeliveryPartner,
+  Admin,
+  Product,
+  Category,
+};
